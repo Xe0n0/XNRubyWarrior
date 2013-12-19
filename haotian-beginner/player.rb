301d50b312e64a5b83b3292b8c2cce030f7bdb45
wuhaotian <thelongestmailaccounteverexist@gmail.com>
2013-12-19 18:41:46 +0800

    revise level 2

--- haotian-beginner/player.rb
+++ haotian-beginner/player.rb
@@ -1,21 +1,29 @@
 class Player
+
   def initialize()
     @enemies = 4
+    @last_action = :safe
   end
+
   def play_turn(warrior)
-    puts "new turn"
+
     if not warrior.feel.empty?
         warrior.attack!
-        if warrior.feel.empty?
-          @enemies -= 1
-          puts @enemies
-        end
+        @last_action = :attack
     else
+
+      if @last_action == :attack
+        @enemies -= 1
+        @last_action = :safe
+      end
+
       if warrior.health < 7 and @enemies > 0
         warrior.rest!
         return
       end
+
       warrior.walk!
+
     end
   end
 end
